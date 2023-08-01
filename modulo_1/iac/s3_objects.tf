adad1b1e39fdbf84497ff17227196cd3bdf821f8
Eder Nogueira <eder.nogueira@thoughtworks.com>
2023-08-01 17:30:01 -0300

    Create new Terraform resources: S3bucket, S3 objects, Lambda function, IAM role and policy

--- modulo_1/iac/s3_objects.tf
+++ modulo_1/iac/s3_objects.tf
@@ -5,3 +5,19 @@ resource "aws_s3_bucket_object" "codigo_spark" {
   source = "../glue-job-imdb-movies.py"
   etag   = filemd5("../glue-job-imdb-movies.py")
 }
+
+resource "aws_s3_bucket_object" "delta_insert" {
+  bucket = aws_s3_bucket.datalake.id
+  key    = "emr-code/pyspark/job_spark/01_delta_spark_insert.py"
+  acl    = "private"
+  source = "../etl/01_delta_spark_insert.py"
+  etag   = filemd5("../etl/01_delta_spark_insert.py")
+}
+
+resource "aws_s3_bucket_object" "delta_upsert" {
+  bucket = aws_s3_bucket.datalake.id
+  key    = "emr-code/pyspark/job_spark/02_delta_spark_upsert.py"
+  acl    = "private"
+  source = "../etl/02_delta_spark_upsert.py"
+  etag   = filemd5("../etl/02_delta_spark_upsert.py")
+}
