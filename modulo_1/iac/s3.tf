7a8102d2ce4277d80886399f78b0aadb85013617
Eder Nogueira <eder.nogueira@thoughtworks.com>
2023-08-01 22:38:57 -0300

    create new tf resources for kinesis poc

--- modulo_1/iac/s3.tf
+++ modulo_1/iac/s3.tf
@@ -13,3 +13,21 @@ resource "aws_s3_bucket" "datalake" {
     CURSO = "EDC"
   }
 }
+
+resource "aws_s3_bucket" "stream" {
+  bucket = "datalake-eder-igti-edc-stream"
+  acl    = "private"
+
+  tags = {
+    IES   = "IGTI",
+    CURSO = "EDC"
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
