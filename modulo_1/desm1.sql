24a364a1c78fcc1dd9495998d5b9b83b2767406a
Eder Nogueira <eder.nogueira@thoughtworks.com>
2023-08-03 12:10:46 -0300

    start challenge 1 module 1 - change tf resources

---
+++ modulo_1/desm1.sql
@@ -0,0 +1,53 @@
+-- Q1
+-- Qual é o SEGUNDO motivo de desligamento mais frequente?
+
+-- Q2
+-- Qual é o estado com a maior média de renda nominal do Brasil?
+
+-- Q3
+-- Qual é a média da renda nominal da UF 31 (utilize a renda média nominal)?
+
+-- Q4
+-- Quantas pessoas, no estado 21, possuem renda nominal média menor do que o salário-mínimo de 2020 (1039 reais)?
+
+-- Q5
+-- Qual foi a média de horas trabalhadas na UF 16?
+
+-- Q6
+-- Qual é a diferença entre os salários médios nominais entre categorias de sexo?
+
+-- Q7
+-- Qual é a diferença entre os salários médios nominais entre categorias de sexo para os trabalhadores da área de tecnologia (CNAE 2.0 Classe = 62040)?
+
+-- Q8
+-- Qual dos comandos abaixo não é estritamente necessário (embora importante) para o deploy de infraestrutura via Terraform?
+-- R: B
+
+-- Q9 
+-- Para que serve o arquivo .tfstate do Terraform?
+-- R: B
+
+-- Q10
+--  Utilizar IaC para o deploy automatizado da infraestrutura da solução está mais relacionado a qual princípio do Well Architected Framework?
+-- R: D
+
+-- Q11
+-- Qual dos formatos de arquivo é o mais recomendado para utilização em zona de consumo do Data Lake?
+-- R: C
+
+-- Q12
+-- Podem ser consideradas Engines de Data Lake, EXCETO:
+-- R: A
+
+-- Q13
+-- Para realizar a ingestão de dados em tempo real numa plataforma em nuvem, podemos utilizar as ferramentas abaixo, EXCETO:
+-- R: C
+
+-- Q14
+-- O serviço serverless AWS Lambda (serviço de computação sem servidor) está relacionado a qual modelo de serviço em nuvem?
+-- R: A
+
+-- Q15
+-- O que caracteriza uma arquitetura Lambda?
+-- R: B
+
--- modulo_1/iac/s3_objects.tf
+++ modulo_1/iac/s3_objects.tf
@@ -1,23 +1,23 @@
-resource "aws_s3_bucket_object" "codigo_spark" {
-  bucket = aws_s3_bucket.datalake.id
-  key    = "emr-code/pyspark/job_spark/job_spark_from_tf.py"
-  acl    = "private"
-  source = "../glue-job-imdb-movies.py"
-  etag   = filemd5("../glue-job-imdb-movies.py")
-}
+# resource "aws_s3_bucket_object" "codigo_spark" {
+#   bucket = aws_s3_bucket.datalake.id
+#   key    = "emr-code/pyspark/job_spark/job_spark_from_tf.py"
+#   acl    = "private"
+#   source = "../glue-job-imdb-movies.py"
+#   etag   = filemd5("../glue-job-imdb-movies.py")
+# }
 
-resource "aws_s3_bucket_object" "delta_insert" {
-  bucket = aws_s3_bucket.datalake.id
-  key    = "emr-code/pyspark/job_spark/01_delta_spark_insert.py"
-  acl    = "private"
-  source = "../etl/01_delta_spark_insert.py"
-  etag   = filemd5("../etl/01_delta_spark_insert.py")
-}
+# resource "aws_s3_bucket_object" "delta_insert" {
+#   bucket = aws_s3_bucket.datalake.id
+#   key    = "emr-code/pyspark/job_spark/01_delta_spark_insert.py"
+#   acl    = "private"
+#   source = "../etl/01_delta_spark_insert.py"
+#   etag   = filemd5("../etl/01_delta_spark_insert.py")
+# }
 
-resource "aws_s3_bucket_object" "delta_upsert" {
-  bucket = aws_s3_bucket.datalake.id
-  key    = "emr-code/pyspark/job_spark/02_delta_spark_upsert.py"
-  acl    = "private"
-  source = "../etl/02_delta_spark_upsert.py"
-  etag   = filemd5("../etl/02_delta_spark_upsert.py")
-}
+# resource "aws_s3_bucket_object" "delta_upsert" {
+#   bucket = aws_s3_bucket.datalake.id
+#   key    = "emr-code/pyspark/job_spark/02_delta_spark_upsert.py"
+#   acl    = "private"
+#   source = "../etl/02_delta_spark_upsert.py"
+#   etag   = filemd5("../etl/02_delta_spark_upsert.py")
+# }
