5103500fe6f59bd34fa2a94dc1ad416c5c881bef
Eder Nogueira <eder.nogueira@thoughtworks.com>
2023-08-03 14:18:03 -0300

    fix glue_job tf definition

--- modulo_1/iac/glue_job.tf
+++ modulo_1/iac/glue_job.tf
@@ -1,6 +1,6 @@
 resource "aws_glue_job" "ftp_to_s3_job" {
   name     = "ftp_to_s3_job"
-  role_arn = aws_iam_role.ftp_to_s3_job.arn
+  role_arn = aws_iam_role.glue_role.arn
 
   command {
     script_location = "s3://${aws_s3_bucket.bucket_rais.id}/scripts/python/transfer-ftp-to-s3.py"
